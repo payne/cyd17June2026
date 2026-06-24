d97b93b69fccbfa9e6cea07851880665b8f180bf
Matt Payne <Payne@MattPayne.org>
2026-06-24 18:52:56 -0500

    Use when a fresh CYD arrives.

---
+++ fresh.sh
@@ -0,0 +1,6 @@
+ mpremote connect /dev/ttyUSB0 cp ili9341.py :ili9341.py 
+ mpremote connect /dev/ttyUSB0 cp .env :.env
+ mpremote connect /dev/ttyUSB0 cp clock.py :clock.py
+ mpremote connect /dev/ttyUSB0 cp chuck.py :chuck.py
+ mpremote connect /dev/ttyUSB0 cp main.py :main.py
+ mpremote connect /dev/ttyUSB0 run main.py 
